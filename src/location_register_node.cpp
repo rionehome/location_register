7a1e467e69dbc900499dd6fdd52238b2c5bd96d4
ItoMasaki <l1sum@icloud.com>
2020-02-13 23:22:59 +0900

    write node launcher

---
+++ src/location_register_node.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <rclcpp/rclcpp.hpp>
+
+#include "location_register.hpp"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    rclcpp::init(argc, argv);
+
+    auto node = make_shared<LocationRegister>();
+
+    rclcpp::spin(node);
+
+    rclcpp::shutdown();
+
+    return 0;
+}
